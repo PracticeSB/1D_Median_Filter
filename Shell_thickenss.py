06f597ed285e24d620d6d8b3265039f7a0fa291d
PracticeSB <suny1357913@naver.com>
2023-01-08 02:57:39 +0900

    Shell_thickenss completed

--- Shell_thickenss.py
+++ Shell_thickenss.py
@@ -6,28 +6,33 @@ import math
 R_out, R_in, rho_silver, rho_polymer, WP\
     = sp.symbols('R_out Rin rho_silver rho_polymer WP')
 
-A = rho_silver*math.pi*(R_out**2-R_in**2)
+A = rho_silver*math.pi*((R_out**2)-(R_in**2))
 B = rho_polymer*math.pi*R_in**2
-Equation = sp.Eq(A/(A+B),WP)
+Equation = sp.Eq((A/(A+B)),WP)
+print(Equation)
 #===================================
 
 #Variable value ===================
-R_out_value = 1
-rho_silver_value = 1000
-rho_polymer_value = 100
-WP_value = 20
+R_out_value = 0.5 #Unit: cm
+rho_silver_value = 10.49 #Unit: 10.49g/cm^3
+rho_polymer_value = 0.92 #Unit: 0.92g/cm^3
+WP_value = 0.25
 #===================================
+C_math = sp.solve(Equation,R_in)
+#print(C_math)
 
 C = Equation.subs([(R_out,R_out_value),(rho_silver,rho_silver_value),
                    (rho_polymer,rho_polymer_value),(WP,WP_value)])
-
 Value = sp.solve(C,R_in)
 
-
 Real_Value = []
+
 for i in range(0,len(Value)):
     if Value[i] > 0:
         Real_Value.append(Value[i])
+
 for i in range(0,len(Real_Value)):
-    print('Thickness: ', R_out_value - Real_Value[i])
+    print(Real_Value[i])
+    print('Thickness[mm]: ', (R_out_value - Real_Value[i])*10)
+    print('Ratio:', ((R_out_value - Real_Value[i])/Real_Value[i]))
 
