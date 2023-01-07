adfa370d6417e140ca619171700e8bb9e030a0d5
PracticeSB <suny1357913@naver.com>
2023-01-08 02:23:21 +0900

    Shell_thickness calculation

--- Shell_thickenss.py
+++ Shell_thickenss.py
@@ -2,12 +2,32 @@ import sympy as sp
 import numpy as np
 import math
 
-R_out, R_in, rho_silver, rho_polymer, m1, m2, WP\
-    = sp.symbols('R_out Rin rho_silver rho_polymer m1 m2 WP')
+#Variable assign && Formula ===================
+R_out, R_in, rho_silver, rho_polymer, WP\
+    = sp.symbols('R_out Rin rho_silver rho_polymer WP')
 
 A = rho_silver*math.pi*(R_out**2-R_in**2)
 B = rho_polymer*math.pi*R_in**2
 Equation = sp.Eq(A/(A+B),WP)
-print(sp.solve(Equation,R_in))
-Thickness = R_out - R_in
+#===================================
+
+#Variable value ===================
+R_out_value = 1
+rho_silver_value = 1000
+rho_polymer_value = 100
+WP_value = 20
+#===================================
+
+C = Equation.subs([(R_out,R_out_value),(rho_silver,rho_silver_value),
+                   (rho_polymer,rho_polymer_value),(WP,WP_value)])
+
+Value = sp.solve(C,R_in)
+
+
+Real_Value = []
+for i in range(0,len(Value)):
+    if Value[i] > 0:
+        Real_Value.append(Value[i])
+for i in range(0,len(Real_Value)):
+    print('Thickness: ', R_out_value - Real_Value[i])
 
