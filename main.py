cfe6ac0b16423b32c6df74732d8e768caba73916
PracticeSB <suny1357913@naver.com>
2023-01-08 00:00:16 +0900

    Weight_percentage

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-## Median filter 만들어보기!
+## 1D Median filter 만들어보기!
+import numpy as np
+
+
+def my_1D_Median(data,k):
+    temp = data
+
+    if k % 2 != 0:
+        print('Windowsize를 홀수로 만들어주세요')
+     np.median()
